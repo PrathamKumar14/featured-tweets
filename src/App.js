7e02a3d3864df61ad5ef6dfc265c7d93e3216e4e
prathamkumar14 <prathamsolera@gmail.com>
2022-04-09 22:58:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    fetch("{custom_functions_server_url}/{project_name}/URL")
+      .then((res) => res.json())
+      .then((data) => setTweets(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Featured Tweets</h1>
+      <h3>
+        A collection of excellent tweets by{" "}
+        <a href="https://twitter.com/prathkum">Pratham</a>
+      </h3>
+      <div className="tweet-container">
+        {tweets.map((tweet, key) => {
+          return (
+            <div className="tweet">
+              <p>{tweet.tweet}</p>
+              <a href={tweet.tweet_url}>Read more ...</a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
